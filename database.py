ccee929938c8319ba8e8f622f0a8bfbe3040e614
nuan ni <87644188+ninuan@users.noreply.github.com>
2023-02-27 13:38:46 +0000

    added jobpage

--- database.py
+++ database.py
@@ -16,10 +16,21 @@ def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     # print(result.all())
-    jobs = []
-    for row in result.all():
-      # print(row._mapping)
-      jobs.append(row._mapping)
+    jobs = [row._asdict() for row in result.all()]
+    # for row in result.all():
+    #   # print(row._mapping)
+    #   jobs.append(row._mapping)
   
     return jobs
-  
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = " + id))
+    rows = result.all()
+    # print(rows[0])
+    if len(rows) == 0:
+      return None
+    else:
+      row_dict = [rows[0]._asdict()]
+      # print(row_dict)
+      return row_dict
